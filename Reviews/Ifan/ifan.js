499422933f51bd8ff8407bf34dd902bd7e4308a8
LewisTheDev <lewismassey4@gmail.com>
2021-06-10 21:31:09 +0100

    added nav.js testing js links

--- Reviews/Ifan/ifan.js
+++
@@ -1,6 +0,0 @@
-
-const changeSebille = function sebillePage() {
-    location.replace("C:\Users\lewis\Desktop\Divinity Project 1\Reviews\Sebille\sebille.html")
-}
-
-document.getElementById(#ifan)
---
+++ Reviews/nav.js
@@ -0,0 +1,8 @@
+
+const clickSebille = document.querySelector('#buttonSebille')
+
+clickSebille.onclick = function () {
+    location.replace("C:\Users\lewis\Desktop\Divinity Project 1\Reviews\Sebille\sebille.html")
+}
+
+
