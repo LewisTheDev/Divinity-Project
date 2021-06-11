ac866c32c9604a2791796d024ae25e74cde77f7f
LewisTheDev <lewismassey4@gmail.com>
2021-06-11 14:29:41 +0100

    added in other origin character pages. nav js functionality done. Templete ready for now

--- Reviews/nav.js
+++ Reviews/nav.js
@@ -1,6 +1,10 @@
 
 const buttonSebille = document.getElementById("buttonSebille");
 const buttonIfan = document.getElementById("buttonIfan");
+const buttonFane = document.getElementById("buttonFane");
+const buttonLohse = document.getElementById("buttonLohse");
+const buttonBeast = document.getElementById("buttonBeast");
+const buttonRedPrince = document.getElementById("buttonRedPrince")
 
 buttonSebille.onclick = function () {
     location.href = ("./sebille.html");
@@ -9,3 +13,19 @@ buttonSebille.onclick = function () {
 buttonIfan.onclick = function () {
     location.href = ("./ifan.html");
 };
+
+buttonFane.onclick = function () {
+    location.href = ("./fane.html");
+}
+
+buttonLohse.onclick = function () {
+    location.href = ("./lohse.html");
+}
+
+buttonBeast.onclick = function () {
+    location.href = ("./beast.html");
+}
+
+buttonRedPrince.onclick = function () {
+    location.href = ("./redprince.html");
+}
